74c85a82b078c423842d55b0f76292a31c7f0913
gunjan <gunjan@SIMPRO-034>
2018-03-05 12:34:59 +0530

    Model Validation

--- ERM.Core/Model/Login.cs
+++ ERM.Core/Model/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace ERM.Core.Model
 {
    public class Login
     {
+      
         public int LogInId { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string UserPassword { get; set; }
     }
 }
